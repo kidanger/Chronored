faaf60c3d84354b63e5a98a3bffb798e6b13d4fc
kidanger <angerj.dev@gmail.com>
2013-12-30 17:02:51 +0100

    Update to latest drystal

--- update_page.sh
+++ update_page.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 cd ../drystal
-./runner.py clean
-./runner.py chronored repack
-cd chronored
+./runner.py repack ../chronored -i -d ../chronored/web
+cd ../chronored
 
 git checkout gh-pages || exit
-cp ../drystal/build-web/*compress* .
-cp ../drystal/build-web/index.html .
-du -sh *
-git commit index.html *compress* -m "update pages"
+
+du -sh web
+mv web/* .
+rmdir web
+
+git commit index.html *compress* *js* *data* -m "update pages"
 git push origin gh-pages
 git checkout master
